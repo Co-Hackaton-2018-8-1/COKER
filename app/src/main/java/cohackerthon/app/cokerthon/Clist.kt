bb3e9d7c68ee5cdc2b6b836e0284d11c339c7738
KimGGun <rlarjs2020@dgsw.hs.kr>
2018-08-08 02:23:52 +0900

    2시

--- app/src/main/java/cohackerthon/app/cokerthon/Clist.kt
+++ app/src/main/java/cohackerthon/app/cokerthon/Clist.kt
@@ -1,7 +1,9 @@
 package cohackerthon.app.cokerthon
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_clist.*
 
 class Clist : AppCompatActivity() {
 
@@ -9,6 +11,19 @@ class Clist : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clist)
 
+        door_btn.setOnClickListener {
+            val nextIntent = Intent(this, Detail::class.java)
+            startActivity(nextIntent)
+        }
 
+        high_btn.setOnClickListener {
+            val nextIntent = Intent(this, Detail2::class.java)
+            startActivity(nextIntent)
+        }
+
+        people_btn.setOnClickListener {
+            val nextIntent = Intent(this, Detail3::class.java)
+            startActivity(nextIntent)
+        }
     }
 }
